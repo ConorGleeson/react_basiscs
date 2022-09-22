bb370dd2eed06cb0c97d7fb0b7a2817d40da76da
Conor Gleeson <conorgleeson2001@gmail.com>
2022-09-22 19:57:17 +0100

    Exercises done apart from fix the final one

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -2,9 +2,43 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
+
+const modules = [
+  {
+    name: "Dev Ops",
+    noLectures: 2,
+    noPracticles: 3,
+  },
+  {
+    name: "Enterprise Web Dev",
+    noLectures: 2,
+    noPracticles: 2,
+
+  }
+]
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <td>{modules[0].name}</td>
+        <td>{modules[0].noLectures}</td>
+        <td>{modules[0].noPracticles}</td>
+      </tr>
+      <tr>
+        <td>{modules[1].name}</td>
+        <td>{modules[1].noLectures}</td>
+        <td>{modules[1].noPracticles}</td>
+      </tr>
+    </tbody >
+  </table>
   );
 };
 
--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -2,10 +2,37 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
+  const list = props.modules.map((i, index) =>
+  <tr key = {index}>
+    <a href={i.url}> {i.name}</a> 
+    <a href={i.url}>{i.noLectures}</a>
+    <a href={i.url}>{i.noPracticals}</a> 
+  </tr>
+  ); 
   return (
-      <h1>TODO</h1>
+    <>
+    <h2>{props.course}</h2>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No. Lectures</th>
+          <th>No. Practicles</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+          {list}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    </>
   );
+  
 };
 
 export default Demo;
